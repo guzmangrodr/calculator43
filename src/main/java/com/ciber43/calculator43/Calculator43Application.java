98093ed130ca7925047e257518cc32415e3630b7
“Guzman <VPX75498@educastur.es>
2022-05-06 20:06:37 +0000

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber43/calculator43/Calculator43Application.java
@@ -0,0 +1,13 @@
+package com.ciber43.calculator43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator43Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber43/calculator43/Calculator43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber43.calculator43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator43ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
